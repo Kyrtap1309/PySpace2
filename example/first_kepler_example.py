1bb9dd60e6d3e146073167254a49d66bfa04d943
Patryk Niczke <kyrtap1301@gmail.com>
2024-09-30 21:03:33 +0200

    Add FirstKepler example and bump version
    Bump version due to properties fix in FirstKepler class

---
+++ example/first_kepler_example.py
@@ -0,0 +1,20 @@
+#Solar System Barycentre calculation with relation to the Sun
+
+#Import PySpace2 interface
+from PySpace2 import PySpace2
+
+
+#Choose when to start analysis
+date = {"year": 2001, "month": 9, "day": 13, "hour": 5, "minute": 0, "second": 0}
+
+#Choose how many days analysis must cover
+delta_days = 1000
+
+#Make Solar System barycentre  object
+first_kepler = PySpace2.first_kepler(delta_days, date)
+
+#Solar System Barycentre info
+print(first_kepler)
+
+#Get array with Solar System Barycentre data
+ssb_data_array = first_kepler.solar_system_barycentre_pos_array
