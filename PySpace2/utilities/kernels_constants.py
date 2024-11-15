528e5355a172bf1afd57a844716470f86d77aa30
Patryk Niczke <kyrtap1301@gmail.com>
2024-11-15 11:05:35 +0100

    Add missing kernel files to orbit calculations

--- PySpace2/utilities/kernels_constants.py
+++ PySpace2/utilities/kernels_constants.py
@@ -19,5 +19,11 @@ REQUIRED_FILES = {
         "pck/pck00010.tpc",
     ],
     "map": ["spk/de432s.bsp", "lsk/naif0012.tls"],
-    "orbit" : ["general/codes_300ast_20100725.bsp", "general/codes_300ast_20100725.tf"]
+    "orbit": [
+        "spk/de432s.bsp",
+        "lsk/naif0012.tls",
+        "pck/gm_de431.tpc",
+        "general/codes_300ast_20100725.bsp",
+        "general/codes_300ast_20100725.tf",
+    ],
 }
