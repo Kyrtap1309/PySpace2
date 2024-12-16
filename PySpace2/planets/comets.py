0686cf46caadc5c283839e43dc63c819980d93fb
Patryk Niczke <kyrtap1301@gmail.com>
2024-12-16 19:14:02 +0100

    Add description method to display statistics about comets

--- PySpace2/planets/comets.py
+++ PySpace2/planets/comets.py
@@ -27,3 +27,36 @@ class Comets:
             'SELECT APHELION_AU, INCLINATION_DEG FROM comets_main WHERE ORBIT_TYPE="C"',
             _conn,
         )
+
+    def description(self, type: str) -> str:
+        """
+        Return the statistics of chosen type of comets
+
+        Parameters:
+        -----------
+        type : str
+            Type of comets 
+
+        Return:
+        -------
+        description : str
+            Statistcs of chosen type of comets
+        """
+        comet_dataframes = {"P": self.p_type_def, "C": self.c_type_def}
+        chosen_dataframe = comet_dataframes[type]
+
+        if type == "P":
+            description = f"""
+            Statistics of P type comets:
+            {self.p_type_def.describe()} \n
+            """
+        elif type == "C":
+            description = f"""
+            Statistics of C type comets with an eccentricity (bound) < 1:
+            {self.c_type_def.loc[self.c_type_def["ECCENTRICITY"]<1].describe()} \n
+
+            Statistics of C type comets with an eccentricity (unbound) >= 1:
+            {self.c_type_def.loc[self.c_type_def["ECCENTRICITY"]>=1].describe()} \n
+            """
+
+        return description
